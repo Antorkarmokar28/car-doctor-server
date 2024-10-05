d42cca0eac7b437cf4abb80d72471cfde7f85cf7
Antorkarmokar28 <antorkarmokar28@gmail.com>
2024-10-05 16:07:43 +0100

    if user verifyed then user read your data and a user can not access another user data access

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // medailware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pctvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,7 +25,23 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+// create a middleware for verifyToken
+const verifyToken = async (req, res, next) =>{
+    const token = req.cookies?.token;
+    if(!token){
+        return res.status(401).send({message: 'Unauthorized'});
+    }
+    // user verify token
+    jwt.verify(token, process.env.USER_SCREETE_TOKEN, (err, decoded) => {
+        // error
+        if(err){
+            return res.status(401).send({message: 'Unauthorized access'});
+        }
+        // if user is valid then it would decoded
+        req.user = decoded;
+        next()
+    })
+}
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,8 +52,13 @@ async function run() {
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             console.log(user);
-            const token = jwt.sign(user, 'secreet', { expiresIn: '1h' });
-            res.send(token)
+            const token = jwt.sign(user, process.env.USER_SCREETE_TOKEN, {
+                expiresIn: '1h',
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+            }).send({ success: true })
         })
         // services related api
         app.get('/services', async (req, res) => {
@@ -58,8 +84,16 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking);
             res.send(result)
         })
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            console.log('user in the valid token', req.user);
+
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+
             let query = {}
+            // console.log('ttt token', req.cookies.token)
             if (req.query?.email) {
                 query = { email: req.query.email }
             }
